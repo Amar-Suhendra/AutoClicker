7b2cbe7fc4d5f9293db60a78399189c452ae4a0f
Amar Suhendra <amarsuhendra2@gmail.com>
2022-08-07 04:54:50 +0700

    update new views

--- AutoClicker/AutoClicker/Form1.Designer.cs
+++ AutoClicker/AutoClicker/Form1.Designer.cs
@@ -36,13 +36,15 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label3 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.leftClickRbt = new System.Windows.Forms.RadioButton();
+            this.rightClickRbt = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(12, 111);
+            this.button1.Location = new System.Drawing.Point(12, 159);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(321, 40);
             this.button1.TabIndex = 0;
@@ -53,7 +55,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 71);
+            this.label1.Location = new System.Drawing.Point(4, 128);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(90, 25);
             this.label1.TabIndex = 1;
@@ -77,7 +79,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(289, 76);
+            this.label3.Location = new System.Drawing.Point(281, 133);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 20);
             this.label3.TabIndex = 5;
@@ -86,23 +88,49 @@
             // numericUpDown1
             // 
             this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numericUpDown1.Location = new System.Drawing.Point(108, 65);
+            this.numericUpDown1.Location = new System.Drawing.Point(100, 122);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(173, 31);
             this.numericUpDown1.TabIndex = 6;
+            // 
+            // leftClickRbt
+            // 
+            this.leftClickRbt.AutoSize = true;
+            this.leftClickRbt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.leftClickRbt.Location = new System.Drawing.Point(26, 60);
+            this.leftClickRbt.Name = "leftClickRbt";
+            this.leftClickRbt.Size = new System.Drawing.Size(79, 17);
+            this.leftClickRbt.TabIndex = 7;
+            this.leftClickRbt.TabStop = true;
+            this.leftClickRbt.Text = "Left Click";
+            this.leftClickRbt.UseVisualStyleBackColor = true;
+            // 
+            // rightClickRbt
+            // 
+            this.rightClickRbt.AutoSize = true;
+            this.rightClickRbt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightClickRbt.Location = new System.Drawing.Point(246, 60);
+            this.rightClickRbt.Name = "rightClickRbt";
+            this.rightClickRbt.Size = new System.Drawing.Size(87, 17);
+            this.rightClickRbt.TabIndex = 8;
+            this.rightClickRbt.TabStop = true;
+            this.rightClickRbt.Text = "Right Click";
+            this.rightClickRbt.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(345, 163);
+            this.ClientSize = new System.Drawing.Size(354, 211);
+            this.Controls.Add(this.rightClickRbt);
+            this.Controls.Add(this.leftClickRbt);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MaximumSize = new System.Drawing.Size(361, 202);
+            this.MaximumSize = new System.Drawing.Size(370, 250);
             this.Name = "Form1";
             this.Text = "Auto Clicker App";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -120,6 +148,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RadioButton leftClickRbt;
+        private System.Windows.Forms.RadioButton rightClickRbt;
     }
 }
 
